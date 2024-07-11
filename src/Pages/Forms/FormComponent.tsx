c0d3345f517b678f649c641fd7e9273fbfcb0485
Bogdan Delic <bogdanworkdelic@gmail.com>
2024-07-11 13:44:25 +0200

    Added check for confirm password, added styling for title and form. Form still needs fixed styling for mobile devices

--- src/Pages/Forms/FormComponent.tsx
+++ src/Pages/Forms/FormComponent.tsx
@@ -30,6 +30,9 @@ const initialFormData: FormData = {
 
 const FormComponent: React.FC<FormComponentProps> = ({ onSubmit }) => {
   const [formData, setFormData] = useState<FormData>(initialFormData);
+  const [passwordMatchError, setPasswordMatchError] = useState<string | null>(
+    null
+  );
   const navigate = useNavigate();
 
   const handleIconClick = () => {
@@ -45,114 +48,136 @@ const FormComponent: React.FC<FormComponentProps> = ({ onSubmit }) => {
       setFormData({ ...formData, [name]: files[0] });
     } else {
       setFormData({ ...formData, [name]: value });
+      if (name === "password" || name === "confirmPassword") {
+        validatePasswordMatch(name, value);
+      }
+    }
+  };
+
+  const validatePasswordMatch = (name: string, value: string) => {
+    if (name === "password") {
+      setPasswordMatchError(
+        formData.confirmPassword !== value ? "Passwords do not match" : null
+      );
+    } else if (name === "confirmPassword") {
+      setPasswordMatchError(
+        formData.password !== value ? "Passwords do not match" : null
+      );
     }
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    onSubmit(formData);
-    clearForm();
-    navigate("/bibliotekari");
+    const { password, confirmPassword } = formData;
+    if (password !== confirmPassword) {
+      setPasswordMatchError("Passwords must match");
+    } else {
+      onSubmit(formData);
+      clearForm();
+      navigate("/bibliotekari");
+    }
   };
 
   const clearForm = () => {
     setFormData(initialFormData);
+    setPasswordMatchError(null);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="firstName">First Name:</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={formData.firstName}
-          onChange={handleChange}
-          required
-        />
+      <div className="form-left-side">
+        <div className="form-group">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">Phone:</label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        {passwordMatchError && (
+          <div className="password-match-error">{passwordMatchError}</div>
+        )}
       </div>
-      <div className="form-group">
-        <label htmlFor="lastName">Last Name:</label>
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-          required
-        />
+      <div className="form-rigth-side">
+        <div className="form-group">
+          <label htmlFor="picture">Profile Picture:</label>
+          <i className="bi bi-image" onClick={handleIconClick}></i>
+          <input
+            type="file"
+            id="picture"
+            name="picture"
+            accept="image/*"
+            onChange={handleChange}
+            style={{ display: "none" }}
+          />
+        </div>
       </div>
-      <div className="form-group">
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
+      <div className="submit-clear">
+        <Button type="submit" className="submit-button">
+          Submit <i className="bi bi-check"></i>
+        </Button>
+        <Button type="button" onClick={clearForm} className="clear-button">
+          Clear <i className="bi bi-x"></i>
+        </Button>
       </div>
-      <div className="form-group">
-        <label htmlFor="phone">Phone:</label>
-        <input
-          type="text"
-          id="phone"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="confirmPassword">Confirm Password:</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="picture">Profile Picture:</label>
-        <i
-          className="bi bi-image"
-          onClick={handleIconClick}
-          style={{
-            cursor: "pointer",
-            border: "1px solid black",
-            padding: "5rem 4rem",
-          }}
-        ></i>
-        <input
-          type="file"
-          id="picture"
-          name="picture"
-          accept="image/*"
-          onChange={handleChange}
-          style={{ display: "none" }}
-        />
-      </div>
-      <Button type="submit" className="submit-button">
-        Submit
-      </Button>
-      <Button type="button" onClick={clearForm} className="clear-button">
-        Clear
-      </Button>
     </form>
   );
 };
